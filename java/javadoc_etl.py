6fd9fc563780bea2d02323d2fbd0a80ed51d6ab7
Apoorv Gupta <ocumancer@gmail.com>
2024-09-16 06:54:28 -0400

    Updates the Java parser to handle the current HTML layout of the Java documentation

--- java/javadoc_etl.py
+++ java/javadoc_etl.py
@@ -5,22 +5,43 @@ import os
 
 def extract(f):
   soup = BeautifulSoup(f, 'html.parser')
-  b = soup.body
+  class_name = soup.head.title.get_text().partition(" ")[0]
+  # Find the main container
+  container = soup.find('div', id='method-summary-table')
   cards = []
-  for table in b.find_all('table',  class_ = "summary-table"):
-      trs = table.tbody.find_all('tr')
-      for tr in trs:
-          card = [soup.head.title.get_text().partition(" ")[0]]
-          for col in tr.children:
-              if str(type(col)) != '<class \'bs4.element.Tag\'>':
-                  continue
-              card.append(col.get_text().replace("\n", " ").replace(";", ","))
-          if len(card) == 4:
-            card = [card[0], card[1] + " " + card[2], card[3]]
-          card = [card[0]+"::"+card[1], card[0] + "::" + card[2]]
-          cards.append(card)
 
+  if container:
+    # Extract table headers
+    headers = container.select('.table-header')
+
+    # Extract method information rows
+    methods = container.select('.col-first, .col-second, .col-last')
+
+    def extract_method_signature(method_element):
+        method_name = method_element.text.strip().replace('\n', ' ')
+        method_params = method_element.find('code')
+        # Handle cases where there's no code element
+        if method_params:
+            full_method = f"{method_name}({method_params.text.strip()})"
+        else:
+            full_method = method_name
+        return method_name
+
+
+    # Print the extracted data
+    for i in range(0, len(methods), 3):
+        return_type = extract_method_signature(methods[i])
+        if (return_type == 'Modifier and Type'):
+            continue
+        method = extract_method_signature(methods[i+1])
+        description = methods[i+2].text.strip().replace('\n', ' ')
+        method_prompt = class_name + "::" + return_type + " " + method
+        method_description = class_name + "::" + description
+        cards.append([method_prompt, method_description, class_name])
+  else:
+    print("No method summary table found in " + class_name)
   return cards
+
 
 def load(cards):
   os.makedirs('./generated/', mode = 0o777, exist_ok = True)
@@ -28,15 +49,19 @@ def load(cards):
   with open('./generated/collections.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=';',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvwriter .writerows(cards)
+    # https://docs.ankiweb.net/importing/text-files.html
+    csvwriter.writerows(cards)
 
 def main():
   cards = []
 # TODO: this is the input file, which should be replaced if you are not interested in collection_framework
-  with open("collection_framework/included", "r") as classnames:
+  included_libraries = "collection_framework/included"
+  with open(included_libraries, "r") as classnames:
+    print('found file ' + included_libraries)
     for classname in classnames.readlines():
         filename = os.path.join(os.path.join(os.path.expanduser("~") ,"Downloads/docs/api/java.base/"), classname[:-1])
         with open(filename, "r") as f:
+          print('found file ' + filename)
           cards.extend(extract(f))
         
   load(cards)
